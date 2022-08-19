9a22f9d73c3901f4dd50adc9fc41fb503960bc54
yakuznecov <yakuznecov@mail.ru>
2022-08-19 22:04:54 +0300

    add Cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+	// состояние модалки
+	const [cartIsShown, setCartIsShown] = useState(false);
+
+	// показ модалки
+	const showCartHandler = () => {
+		setCartIsShown(true);
+	}
+
+	// скрытие модалки
+	const hideCartHandler = () => {
+		setCartIsShown(false);
+	}
+
 	return (
 		<Fragment>
+			<Cart />
 			<Header />
 			<main>
 				<Meals />
